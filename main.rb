b6cc101a4f179ed3187025af20b53720e3166071
oikawa <oikenfight@gmail.com>
2015-11-05 19:41:56 +0900

    add color-red

--- main.rb
+++ main.rb
@@ -1,7 +1,8 @@
 require "./lib/actor/human"
+require "./lib/decorator/color_red"
 require "./lib/decorator/character"
 
 actor = Actor::Human.new 'My name is Hoge Hoge'
-decorator = Decorator::Character.new actor
-
+# decorator = Decorator::Character.new actor
+decorator = Decorator::ColorRed.new actor
 puts decorator.show_message
